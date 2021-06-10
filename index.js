1d30645a8792dbd56078e628c6678d5757ce036f
Dor <dorzairi@ymail.com>
2021-06-10 17:26:23 -0400

    fixed the if being out of scope

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// require fs
+const fs = require("fs");
+
 // require the discord.js module
 const Discord = require("discord.js");
 
@@ -10,6 +13,14 @@ dotenv.config();
 
 // create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -23,8 +34,18 @@ client.on("message", message => {
   const args = message.content
     .slice(prefix.length)
     .trim()
-    .split(" ");
+    .split(/ +/);
   const command = args.shift().toLowerCase();
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.log(error);
+    message.reply(
+      `there was an erorr trying to execute that command! here is the error ${error}`
+    );
+  }
 });
 
 // login to Discord with your app's token
