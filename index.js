501f91351bf1de8f97ba49211ca9a95b77be7937
Dor <dorzairi@ymail.com>
2021-06-10 14:34:36 -0400

    Hiding scerets and doing boops

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // require the discord.js module
 const Discord = require("discord.js");
 
+//require config file
+const { prefix, token } = require("./config.json");
+
+//require .env file
+const dotenv = require("dotenv");
+dotenv.config();
+
 // create a new Discord client
 const client = new Discord.Client();
 
@@ -11,10 +18,12 @@ client.once("ready", () => {
 });
 
 client.on("message", message => {
-  if (message.content === "!ping") {
+  if (message.content === `${prefix}ping`) {
     message.channel.send("pong");
+  } else if (message.content === `${prefix}beep`) {
+    message.channel.send("Boop");
   }
 });
 
 // login to Discord with your app's token
-client.login(my-token);
+client.login(token);
