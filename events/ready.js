a403c428e94e5960b6594d921c8555a4458aba7a
Dor <dorzairi@ymail.com>
2021-06-11 16:49:19 -0400

    having a funcational bot but i want to make it on emojis and reactions

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = bot => {
+  console.log("Bot is online!");
+  console.log("and ready to trade!");
+};
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const Discord = require("discord.js");
-const { token } = require("./config.json");
-const fs = require("fs");
+const { token, MongoDB } = require("./config.json");
+const mongoose = require("mongoose");
+const { loadCommands } = require("./utils/loadCommands");
+const mongoCurrency = require("discord-mongo-currency");
 
-const client = new Discord.Client();
-
-client.once("ready", () => {
-  console.log("Ready!");
+const bot = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
 
-client.on("message", message => {
-  if (message.content === "!react") {
-    console.log(`reacting`);
-    message.react("👛");
-  } else if (message.content === "!coin") {
-    console.log(`reacting with a coin`);
-    message.react("852684562562547722");
-    message.react(":coin:");
-  }
+mongoose.connect(MongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+mongoose.set("useFindAndModify", false);
 
-client.login(token);
+mongoCurrency.connect(MongoDB);
+
+require("./utils/loadEvents")(bot);
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+bot.snipes = new Map();
+
+loadCommands(bot);
+bot.login(token);
